b5d6e59585a648e4d87359a48c47c3a4ba8fee8e
Amrit Saha <amritsaha607@gmail.com>
2024-07-28 11:06:48 +0530

    create auth page UI

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,12 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormLoginComponent } from '../../components/form-login/form-login.component';
-import { FormSignupComponent } from '../../components/form-signup/form-signup.component';
+import { FormItem } from '../../../types';
+import { FormItemComponent } from '../../components/form-item/form-item.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FormSignupComponent, FormLoginComponent],
+  imports: [FormItemComponent, CommonModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  signupFormItems!: FormItem[];
+  loginFormItems!: FormItem[];
+  activeForm: String = 'login';
+
+  constructor() {
+    this.signupFormItems = [
+      {
+        id: 'name',
+        name: 'name',
+        title: 'Name',
+        placeholder: 'Enter name',
+        inputType: 'text',
+      },
+      {
+        id: 'email',
+        name: 'email',
+        title: 'Email',
+        placeholder: 'Create email ID',
+        inputType: 'email',
+      },
+      {
+        id: 'password',
+        name: 'password',
+        title: 'Password',
+        placeholder: 'Create a password',
+        inputType: 'password',
+      },
+    ];
+
+    this.loginFormItems = [
+      {
+        id: 'login_email',
+        name: 'login_email',
+        title: 'Email',
+        placeholder: 'Enter email ID',
+        inputType: 'email',
+      },
+      {
+        id: 'login_password',
+        name: 'login_password',
+        title: 'Password',
+        placeholder: 'Enter password',
+        inputType: 'password',
+      },
+    ];
+  }
+
+  toggleActiveForm() {
+    if (this.activeForm == 'login') {
+      this.activeForm = 'signup';
+    } else {
+      this.activeForm = 'login';
+    }
+  }
+}
