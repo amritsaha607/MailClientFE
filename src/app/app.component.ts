902b7b03d674e5da54b16cc5f7566e3158f4c965
Amrit Saha <amritsaha607@gmail.com>
2024-08-03 18:46:57 +0530

    set header user initials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from '../types';
 import { HeaderComponent } from './layout/header/header.component';
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,44 @@ import { HeaderComponent } from './layout/header/header.component';
 })
 export class AppComponent {
   title = 'MailClientFE';
+  user!: User;
+  initials = '-_-';
+
+  constructor(private sessionService: SessionService) {}
+
+  ngOnInit() {
+    this.fetchSessionUser();
+  }
+
+  fetchSessionUser() {
+    this.sessionService.getSessionUser()?.subscribe((response) => {
+      if (response.status == 200) {
+        this.user = response.body;
+        this.setInitials();
+      }
+    });
+  }
+
+  setInitials() {
+    if (
+      this.user != null &&
+      this.user != undefined &&
+      this.user.name != null &&
+      this.user.name != undefined
+    ) {
+      this.initials = this.getInitialsFromName(this.user.name);
+    }
+  }
+
+  getInitialsFromName(name: string) {
+    const splitFront = name.split(' ').at(0);
+    const splitBack = name.split(' ').at(-1);
+    if (splitFront != undefined) {
+      this.initials = splitFront[0];
+    }
+    if (splitBack != undefined) {
+      this.initials += splitBack[0];
+    }
+    return this.initials;
+  }
 }
